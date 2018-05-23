84ea6e70d9c8ddd76304d0e71bcd4c9512997a15
Ilya <ilyazuev133@gmail.com>
2018-05-23 14:26:20 +1000

    Add a new parameters for Controller

--- include/controller.h
+++ include/controller.h
@@ -9,17 +9,30 @@
 #include "server.h"
 #include "timer.h"
 #include "message.h"
+#include "messagecontroller.h"
+#include "controltable.h"
+#include "servo.h"
+#include "dynamixel_sdk.h"
 
 class Controller: public QObject {
     Q_OBJECT
 public:
     Controller(QObject *parent = 0);
-    Message::Id *outId;
-    Message::Test *outTest;
+//    Message::Id *outId;
+//    Message::Id *inId;
 private:
-    void write();
     Server *server;
     PTimer timer;
+    MessageController *messageController;
+    dynamixel::PortHandler *portHandler;
+    dynamixel::PacketHandler *packetHandler;
+    std::vector<Servo*> servos;
+
+    Message::Move *inMove;
+
+    std::string deviceName;
+    float protocolVersion;
+    int port;
 public slots:
     void slotWriteStatus();
 };
--- src/controller.cpp
+++ src/controller.cpp
@@ -1,18 +1,43 @@
 #include <QObject>
+#include <QCoreApplication>
+#include <iostream>
+#include <vector>
 
 #include "controller.h"
 
+#include <rapidjson/filereadstream.h>
+#define BAUDRATE                        57600
+using namespace rapidjson;
+
 Controller::Controller(QObject *parent) :
     QObject(parent) {
-    outId = new Message::Id;
-    outTest = new Message::Test;
 
-    server = new Server;
-    server->startServer();
-
+    server = new Server(port);
     timer = PTimer(new Timer());
+
     connect(timer.get(), SIGNAL(finished()), timer.get(), SLOT(deleteLater()));
     connect(timer.get(), SIGNAL(signTimed()), this, SLOT(slotWriteStatus()));
+    messageController = new MessageController(server);
+    inMove = new Message::Move;
+    messageController->addScheme(inMove, Message::Command::CONTROL);
+
+    portHandler = dynamixel::PortHandler::getPortHandler(deviceName.data());
+    packetHandler = dynamixel::PacketHandler::getPacketHandler(protocolVersion);
+
+    if (portHandler->openPort())
+        qDebug() << "Succeeded to open the port!\n";
+    else
+        qDebug() << "Failed to open the port!\n";
+
+    if (portHandler->setBaudRate(BAUDRATE))
+        qDebug() << "Succeeded to change the baudrate!\n";
+    else
+        qDebug() << "Failed to change the baudrate!\n";
+
+//    qDebug() << servoId;
+//    inId->id = servoId;
+
+    server->start();
     timer->start();
 }
 
